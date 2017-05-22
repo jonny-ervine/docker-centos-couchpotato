0eb977f338117ab8155214344164ca499042cdfd
Jonny Ervine <jervine@users.noreply.github.com>
2017-05-22 12:21:24 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,11 +6,15 @@
 ## Initialise any variables being called:
 # Set the correct timezone
 TZ=${TZ:-UTC}
+USER=$USER
+UID=$UID
 setup=/config/.setup
 
 if [ ! -f "${setup}" ]; then
   rm -f /etc/localtime
   cp /usr/share/zoneinfo/$TZ /etc/localtime
+  adduser -u $UID $USER
+  sed -i "s/user=/user=$USER/g" /etc/supervisord.d/couchpotato.ini
   touch $setup
 fi
 
